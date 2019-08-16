999863326aef380e557fc3c3f8a7199f4eae42de
vamsee <vamsee333@gmail.com>
2019-08-16 08:29:15 +0530

    template related changes and view implementation

--- MultiSite/Home/urls.py
+++ MultiSite/Home/urls.py
@@ -1,7 +1,8 @@
-from .views import index,about
+from .views import index,about,Explore
 from django.urls import path
 
 urlpatterns=[
-    path('home/',index,name="index"),
+    path('home/<int:id>',index,name="index"),
     path('about/',about,name='about'),
+    path('explore/',Explore,name='explore'),
 ]
--- MultiSite/Home/views.py
+++ MultiSite/Home/views.py
@@ -3,10 +3,17 @@ from .models import HomePageItems
 # Create your views here.
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("helllo world ")
+def index(request,id):
+    return HttpResponse("helllo world %s " %id)
 
 def about(request):
-    return HttpResponse('this a new hybrid website')
+    items = HomePageItems.objects.all()
+    context={'items':items}
+    return render(request,'homepage.html',context)
+
+def Explore(request):
+    return render(request,'explore.html',{})
+
+
 
 
