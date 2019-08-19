eb8bf74e5c5e2e90ad95d8a80a4d94fac816ef05
vamsee <vamsee333@gmail.com>
2019-08-19 08:27:31 +0530

    modified views nd urls and added text files

--- MultiSite/Home/urls.py
+++ MultiSite/Home/urls.py
@@ -1,8 +1,9 @@
-from .views import index,about,Explore
+from .views import index,about,Explore,showdetail
 from django.urls import path
 
 urlpatterns=[
     path('home/<int:id>',index,name="index"),
     path('about/',about,name='about'),
     path('explore/',Explore,name='explore'),
+    path('faq/',showdetail,name='details'),
 ]
--- MultiSite/Home/views.py
+++ MultiSite/Home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from .models import HomePageItems
+from .models import HomePageItems,FAQ,RelatedAnswers
+from django.shortcuts import get_object_or_404
 # Create your views here.
 from django.http import HttpResponse
+from django.utils import timezone
 
 def index(request,id):
     return HttpResponse("helllo world %s " %id)
@@ -15,5 +17,11 @@ def Explore(request):
     return render(request,'explore.html',{})
 
 
-
+def showdetail(request):
+    items=FAQ.objects.all()
+    date=[]
+    for x in items:
+        date.append(timezone.now().hour - x.added_date.hour)
+    context={'items':items,'hours':date}
+    return render(request,'showdetail.html',context)
 
